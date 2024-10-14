d0d6ca8d26457f9ed4ff60e1a0f5985f74c6ec63
wilfredmanyara <manyarawilly46@gmail.com>
2024-10-14 16:06:46 +0300

    feat(mockvue):add new interview button & functionality

---
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -0,0 +1,94 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+function AddNewInterview() {
+  const [openDialog, setOpenDialog] = useState(false);
+  const [jobPosition, setJobPosition] = useState();
+  const [jobDesc, setJobDesc] = useState();
+  const [jobExperience, setJobExperience] = useState();
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(jobPosition, jobDesc, jobExperience);
+  };
+  return (
+    <div>
+      <div
+        className="p-10 border rounded-lg bg-secondary hover:shadow-md cursor-pointer"
+        onClick={() => setOpenDialog(true)}
+      >
+        <h2 className="text-lg text-center">+ Add New</h2>
+      </div>
+      <Dialog open={openDialog}>
+        <DialogContent className="max-w-2xl">
+          <DialogHeader>
+            <DialogTitle className="text-2xl">
+              Tell us more about your Job Interview
+            </DialogTitle>
+            <DialogDescription>
+              <form onSubmit={onSubmit}>
+                <div>
+                  <h2>
+                    Add Details about your Job Position/role, Job description
+                    and years of experience
+                  </h2>
+
+                  <div className="mt-7 my-3">
+                    <label>Job Position/Role</label>
+                    <Input
+                      placeholder="Ex. Full Stack Developer"
+                      required
+                      onChange={(event) => setJobPosition(event.target.value)}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <label>Job Description/Tech Stack</label>
+                    <Textarea
+                      placeholder="Ex. React, Angular, NodeJs, MySql"
+                      required
+                      onChange={(event) => setJobDesc(event.target.value)}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <label>Years of Experience</label>
+                    <Input
+                      placeholder="Ex. 5"
+                      type="number"
+                      max="50"
+                      required
+                      onChange={(event) => setJobExperience(event.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-5 justify-end">
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    onClick={() => setOpenDialog(false)}
+                  >
+                    Cancel
+                  </Button>
+                  <Button type="submit">Start Interview</Button>
+                </div>
+              </form>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddNewInterview;
