3876462fb688aced9fa8a30724748843fb70655a
wilfredmanyara <manyarawilly46@gmail.com>
2024-10-14 14:50:18 +0300

    feat(mockvue):install drizzle & update db schema

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+/** @type { import("drizzle-kit").Config } */
+
+export default {
+  schema: "./utils/schema.js",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://wilfredmanyara:Eohy5SCV9TRe@ep-plain-shape-a34nshol.eu-central-1.aws.neon.tech/mockvue?sslmode=require",
+  },
+};
